fbe77c2c106a3f6308df79cb76b46391210a03d5
SMerski <merski70@gmail.com>
2022-07-15 23:15:34 +0200

    works even better, could get 300 rows

--- Imports/index.js
+++ Imports/index.js
@@ -15,7 +15,28 @@ const client = new postgres.Client({
     port: 5432
 });
 client.connect().then(() => {
-    console.log('Connected to database');
+    client.query(`CREATE TABLE IF NOT EXISTS matches (
+        matchId VARCHAR(255) NOT NULL PRIMARY KEY,
+        gameTime VARCHAR(255) NOT NULL,
+        summonerId VARCHAR(255) NOT NULL,
+        championId INTEGER NOT NULL,
+        championName VARCHAR(255) NOT NULL,
+        role VARCHAR(255) NOT NULL,
+        lane VARCHAR(255) NOT NULL,
+        kills INTEGER NOT NULL,
+        deaths INTEGER NOT NULL,
+        assists INTEGER NOT NULL,
+        win BOOLEAN NOT NULL,
+        duration INTEGER NOT NULL,
+        CONSTRAINT summoners_id 
+            FOREIGN KEY (summonerId)
+            REFERENCES summoners (summonerId)
+            ON DELETE CASCADE
+        );`)
+    .then(() => {
+
+        console.log('Connected to database');
+    });
 }).catch(err => {
     console.log('Error connecting to database');
 });
@@ -81,10 +102,21 @@ async function importToDatabase(summonerName, start, count){
             ${match.deaths},
             ${match.assists},
             '${match.win}',
-            '${match.duration}')`);
+            '${match.duration}')`).then(() => {
+                return 0;
+            }).catch(err => {
+                console.log(err);
+            });
+        }).catch(err => {
+            console.log(err);
         });
     });
+    
 }
 for (let i = 0; i < 10; i++) {
-    importToDatabase('Meruszka', i*100, 100);
+    importToDatabase('Meruszka', i*50, 50).then(() => {
+        console.log('Imported', i*50, 'to', (i+1)*50);
+    }).catch(err => {
+        console.log(err);
+    });    
 }
